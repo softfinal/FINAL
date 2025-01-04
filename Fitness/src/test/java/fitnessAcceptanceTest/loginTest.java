d4f2bd737728f01d45178a94a4a992f3c3f7d1cc
LoaaAwayes <162191783+LoaaAwayes@users.noreply.github.com>
2025-01-04 20:49:11 +0200

    Update loginTest.java

--- Fitness/src/test/java/fitnessAcceptanceTest/loginTest.java
+++ Fitness/src/test/java/fitnessAcceptanceTest/loginTest.java
@@ -1,7 +1,6 @@
 package fitnessAcceptanceTest;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import java.util.logging.Level;
@@ -11,17 +10,11 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class loginTest{
+public class loginTest {
 	
     User u = new User();
-  boolean loggied=false;
+    boolean loggied=false;
     static Logger logger= Logger.getLogger(loginTest.class.getName());
-
-   
-    @Given("table")
-    public void table_step() {
-        // Step implementation here
-    }
 
     @Given("that the user is not logged in")
     public void that_the_user_is_not_logged_in() {
@@ -39,127 +32,105 @@ public class loginTest{
     @Then("the user is logged in successfully")
     public void the_user_is_logged_in_successfully() {
         u.setLogstate(true);
-        assertTrue( u.getLogstate());
+        assertTrue(u.getLogstate());
     }
 
     @Given("{string} is not in the database")
     public void is_not_in_the_database(String string) {
-
-        if (!User.users1.equals(string)) {
-
-            u.setLogstate(false);
-        }
-
-        assertFalse(u.getLogstate());
-
+        checkUserInDatabase(string, "the username is wrong");
     }
 
     @Then("Show {string} message.")
     public void show_message(String string) {
-        string="the username is wrong";
-        logger.log(Level.INFO,string);
+        logger.log(Level.INFO, String.format("Message: %s", string));
     }
 
     @Given("the {string} not in database")
     public void the_not_in_database(String string) {
-        if (!User.users1.equals(string)) {
-
-            u.setLogstate(false);
-        }
-        assertFalse(u.getLogstate());
+        checkUserInDatabase(string, "the password is wrong");
     }
 
     @Then("Show message {string}")
     public void show_massege(String string) {
-        string ="the password is wrong";
-        logger.log(Level.INFO,string);
+        logger.log(Level.INFO, String.format("Message: %s", string));
     }
 
+    // Helper method to check user data in the database and log message
+    private void checkUserInDatabase(String userInput, String errorMessage) {
+        if (!User.users1.equals(userInput)) {
+            u.setLogstate(false);
+            assertFalse(u.getLogstate());
+            logger.log(Level.INFO, String.format("%s: %s", errorMessage, userInput));
+        }
+    }
 
- @Test
+    // Test cases
+    @Test
     public void testUserLoginWithCorrectCredentials() {
-      
         u.setName("correctUsername");
         u.setPassword("correctPassword");
-
-   
         u.loginCH("correctUsername", "correctPassword");
-
-     
         assertTrue(u.getLogstate());
     }
 
- @Test
- public void testUserLoginWithIncorrectCredentials() {
-     User user = new User();
-     user.setName("incorrectUsername");
-     user.setPassword("incorrectPassword");
-   
-     user.loginCH("correctUsername", "correctPassword");
+    @Test
+    public void testUserLoginWithIncorrectCredentials() {
+        User user = new User();
+        user.setName("incorrectUsername");
+        user.setPassword("incorrectPassword");
+        user.loginCH("correctUsername", "correctPassword");
+        assertFalse(user.getLogstate());
+    }
 
-     assertFalse(user.getLogstate());
- }
+    @Test
+    public void testUserNameAndPasswordNull() {
+        User user = new User();
+        user.setName(null);
+        user.setPassword(null);
+        loggied=false;
+        user.setLogstate(loggied);
+        assertFalse(user.getLogstate());
+    }
 
- @Test
- public void testUserNameAndPasswordNull() {
-     User user = new User();
-     user.setName(null);
-     user.setPassword(null);
-     loggied=false;
-     user.setLogstate(loggied);
-     assertFalse(user.getLogstate());
-   
+    @Test
+    public void testUserNameAndPasswordEmpty() {
+        User user = new User();
+        user.setName("");
+        user.setPassword("");
+        user.loginCH("", "");
+        loggied=false;
+        user.setLogstate(loggied);
+        assertFalse(user.getLogstate());
+    }
 
-     
- }
+    @Test
+    public void testUserNameAndPasswordMismatch() {
+        User user = new User();
+        user.setName("correctUsername");
+        user.setPassword("correctPassword");
+        user.loginCH("correctUsername", "incorrectPassword");
+        assertFalse(user.getLogstate());
+    }
 
- @Test
- public void testUserNameAndPasswordEmpty() {
-     User user = new User();
-     user.setName("");
-     user.setPassword("");
-   
-     user.loginCH("", "");
+    @Test
+    public void testUserNameNotInDatabase() {
+        User user = new User();
+        user.setName("nonexistentUsername");
+        user.setPassword("correctPassword");
+        user.loginCH("nonexistentUsername", "correctPassword");
+        loggied=false;
+        user.setLogstate(loggied);
+        assertFalse(user.getLogstate());
+    }
 
-     loggied=false;
-     user.setLogstate(loggied);
-     assertFalse(user.getLogstate());
- }
-
- @Test
- public void testUserNameAndPasswordMismatch() {
-     User user = new User();
-     user.setName("correctUsername");
-     user.setPassword("correctPassword");
-   
-     user.loginCH("correctUsername", "incorrectPassword");
-
-     assertFalse(user.getLogstate());
- }
-
- @Test
- public void testUserNameNotInDatabase() {
-     User user = new User();
-     user.setName("nonexistentUsername");
-     user.setPassword("correctPassword");
-   
-     user.loginCH("nonexistentUsername", "correctPassword");
-     loggied=false;
-     user.setLogstate(loggied);
-     assertFalse(user.getLogstate());
- }
-
- @Test
- public void testPasswordNotInDatabase() {
-     User user = new User();
-     user.setName("correctUsername");
-     user.setPassword("nonexistentPassword");
-   
-     user.loginCH("correctUsername", "nonexistentPassword");
-
-     loggied=false;
-     user.setLogstate(loggied);
-     assertFalse(user.getLogstate());
- }}
-
-	
+    @Test
+    public void testPasswordNotInDatabase() {
+        User user = new User();
+        user.setName("correctUsername");
+        user.setPassword("nonexistentPassword");
+        user.loginCH("correctUsername", "nonexistentPassword");
+        loggied=false;
+        user.setLogstate(loggied);
+        assertFalse(user.getLogstate());
+    }
+}
